97b113a5724ff4b2177bf487e7654564d81995c4
ayogsynergy <sanchezg.eng@gmail.com>
2024-08-24 14:03:44 -0400

    second batch of journals

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import journalEntryAdapter from "./journalEntryAdapter.js";
 import printToFile from "./markdown/filePrinter.js";
 
 try {
-    const data = fs.readFileSync("../GJournal.json", "utf-8");
+    // const data = fs.readFileSync("../GJournal.json", "utf-8");
+    const data = fs.readFileSync("../Journal.json", "utf-8");
     const json = JSON.parse(data);
     const journalEntries = journal(json);
     journalEntries.forEach(entry => {
